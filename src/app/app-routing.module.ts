03b41242d5ccad2cd8b6b711532fa31f0ac0af19
katerina <sk.katherine@gmail.com>
2017-11-15 17:33:22 +0200

    child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent }   from './welcome/welcome.component';
-import { HomeComponent }   from './home/home.component';
-import { PressComponent }      from './press/press.component';
-import { AboutComponent }  from './about/about.component';
+import { MainComponent }  from './main/main.component';
+import { HomeComponent }   from './main/home/home.component';
+import { PressComponent }      from './main/press/press.component';
+import { AboutComponent }  from './main/about/about.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'press', component: PressComponent },
-  { path: 'about', component: AboutComponent }
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: 'home', component: HomeComponent },
+      { path: 'press', component: PressComponent },
+      { path: 'about', component: AboutComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { HomeComponent } from './home/home.component';
-import { PressComponent } from './press/press.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './main/home/home.component';
+import { PressComponent } from './main/press/press.component';
+import { AboutComponent } from './main/about/about.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { AboutComponent } from './about/about.component';
     WelcomeComponent,
     HomeComponent,
     PressComponent,
-    AboutComponent
+    AboutComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
