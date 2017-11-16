de8d8dc880c0d0b92bc905f945fd898bf2e96b3e
katerina <sk.katherine@gmail.com>
2017-11-16 18:51:17 +0200

    markers

--- src/app/main/home/gmap/gmap.component.ts
+++ src/app/main/home/gmap/gmap.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, Input } from '@angular/core';
 import { GmapService } from '../../../gmap.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-gmap',
@@ -8,6 +9,8 @@ import { GmapService } from '../../../gmap.service';
 })
 
 export class GmapComponent implements AfterViewInit {
+
+  @Input() spaces: Observable<any>;
 
   @ViewChild('mapElement') mapElement: ElementRef;
   @ViewChild('inputElement') inputElement: ElementRef;
@@ -18,45 +21,60 @@ export class GmapComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-      /**
-       * Init map api [google.maps]
-       */
-      this.gapi.init.then((maps: any) => {
-          const loc = new maps.LatLng(38, 23);
 
-          this.map = new maps.Map(this.mapElement.nativeElement, {
-              zoom: 13,
-              center: loc,
-              scrollwheel: true,
-              panControl: false,
-              mapTypeControl: false,
-              zoomControl: true,
-              streetViewControl: false,
-              scaleControl: true,
-              zoomControlOptions: {
-                  style: maps.ZoomControlStyle.LARGE,
-                  position: maps.ControlPosition.RIGHT_BOTTOM
-              }
-          });
+    /**
+     * Init map api [google.maps]
+     */
+    this.gapi.init.then((maps: any) => {
+      const loc = new maps.LatLng(38, 23);
 
-          const input = this.inputElement.nativeElement;
-          const options = {
-              componentRestrictions: {country: 'gr'}
-          };
-
-          const autocomplete = new maps.places.Autocomplete(input, options);
-
-          autocomplete.addListener('place_changed', () => {
-              const place = autocomplete.getPlace();
-              const location = place.geometry.location;
-
-              this.map.setZoom(13);
-              this.map.setCenter({
-                  lat: location.lat(),
-                  lng: location.lng()
-              });
-          });
+      this.map = new maps.Map(this.mapElement.nativeElement, {
+        zoom: 13,
+        center: loc,
+        scrollwheel: true,
+        panControl: false,
+        mapTypeControl: false,
+        zoomControl: true,
+        streetViewControl: false,
+        scaleControl: true,
+        zoomControlOptions: {
+          style: maps.ZoomControlStyle.LARGE,
+          position: maps.ControlPosition.RIGHT_BOTTOM
+        }
       });
+
+      const input = this.inputElement.nativeElement;
+      const options = {
+        componentRestrictions: { country: 'gr' }
+      };
+
+      const autocomplete = new maps.places.Autocomplete(input, options);
+
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        const location = place.geometry.location;
+
+        this.map.setZoom(13);
+        this.map.setCenter({
+          lat: location.lat(),
+          lng: location.lng()
+        });
+      });
+
+        this.spaces.subscribe(x => {
+          console.log(x.length);
+          x.forEach(one => {
+            var pos = {
+              lat: one.location.geometry.coordinates[0],
+              lng: one.location.geometry.coordinates[1]
+            }
+            var marker = new maps.Marker({
+              position: pos,
+              map: this.map
+            })
+          })
+        })
+    });
   }
 
 }
